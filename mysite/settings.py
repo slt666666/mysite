7b8c377cd3f929cac2d207c8f2b22de840b51971
堺俊之 <toshi6661024@gmail.com>
2017-12-31 16:06:56 +0900

    delete sample app

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
