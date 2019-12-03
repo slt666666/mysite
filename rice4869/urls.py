4b4b0aad4a155037db8e3b1b92ab415e82a34533
堺俊之 <toshi6661024@gmail.com>
2019-12-03 21:18:25 +0000

    save now

--- rice4869/urls.py
+++ rice4869/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'rice4869'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('view_detail/', views.view_detail, name='view_detail'),
     path('<str:gene_id>/', views.detail, name='detail'),
     path('<str:gene_id>/network/', views.network, name='network')
 ]
--- rice4869/views.py
+++ rice4869/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, Http404
 from .models import Imochi1
 import numpy as np
 
 
 def index(request):
-    gene_list = Imochi1.objects.order_by('id')[:100]
+    gene_list = Imochi1.objects.order_by('id')[:10]
     context = {
         'gene_list': gene_list,
     }
@@ -20,7 +21,8 @@ def detail(request, gene_id):
 def network(request, gene_id):
     first_gene = get_object_or_404(Imochi1, gene_id=gene_id)
     all_gene = Imochi1.objects.order_by('id')
-    all_gene = all_gene.values_list('rddw_16h', 'rddw_24h', 'rddw_48h', 'rddw_72h')
+    all_gene_index = list(all_gene.values_list('gene_id'))
+    all_gene = all_gene.values_list('rddw_16h', 'rddw_24h', 'rddw_48h', 'rddw_72h', 'rmo_16h', 'rmo_24h', 'rmo_48h', 'rmo_72h', 'sddw_12h', 'sddw_24h', 'sddw_36h', 'sddw_48h', 'sddw_72h', 'smo_12h', 'smo_24h', 'smo_36h', 'smo_48h', 'smo_72h')
     all_gene = np.array(all_gene)
     try:
         second_gene = get_object_or_404(Imochi1, gene_id=request.POST['second_gene'])
@@ -34,4 +36,17 @@ def network(request, gene_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return render(request, 'rice4869/network.html', {'first': first_gene, 'second': second_gene, 'all': all_gene})
+        return render(request, 'rice4869/network.html', {'first': first_gene, 'second': second_gene, 'all': all_gene, 'index': all_gene_index})
+
+
+def view_detail(req):
+    if req.method == 'POST':
+        gene = req.POST['select_gene']
+        surprise_txt = gene + "!!!"
+
+        response = {'your_surprise_txt': surprise_txt, }
+
+        return JsonResponse(response)
+
+    else:
+        raise Http404
