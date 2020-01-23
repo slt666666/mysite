934de8bee40e9f8fde1956279d7ed9710cde304f
堺俊之 <toshi6661024@gmail.com>
2020-01-23 22:44:59 +0000

    add list

--- clusters/views.py
+++ clusters/views.py
@@ -3,8 +3,11 @@ from django.template import Context
 
 
 def index(request):
-    specie_list = Context({"specie_name": ["Tomato_v3_2", "Tomato_v2_4"]})
-    return render(request, 'clusters/index.html', specie_list)
+    specie_list = ["Tomato_v3_2", "Tomato_v2_4"]
+    context = {
+        'specie_list': specie_list,
+    }
+    return render(request, 'clusters/index.html', context)
 
 # def detail(request, specie_name):
 #     return render(request, 'clusters/{}_cluster.html'.format(specie_name))
