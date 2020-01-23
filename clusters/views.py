28a88c2b621d69cad6f0073203635b400824e090
堺俊之 <toshi6661024@gmail.com>
2020-01-23 23:28:57 +0000

    add

--- clusters/views.py
+++ clusters/views.py
@@ -5,6 +5,3 @@ def index(request):
         'specie_list': ["Tomato_v3_2", "Tomato_v2_4"],
     }
     return render(request, 'clusters/index.html', context)
-
-def detail(request, specie_name):
-    return render(request, 'clusters/index.html')
