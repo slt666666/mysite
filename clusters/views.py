b34277ae55c5ab6e2c0e0a9d412b41b364408178
堺俊之 <toshi6661024@gmail.com>
2020-01-23 23:52:12 +0000

    add

--- clusters/views.py
+++ clusters/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, get_object_or_404
 
 def index(request):
     context = {
-        'specie_list': ["Tomato_v3_2", "Tomato_v2_4"],
+        'specie_list': ["Brapa","Arabidopsis","Ashtree","Bentham_v0_4_4","Buckwheat","Cacao","Carrot","Cassava",
+                        "Coffee","Cucumber","Eucalyptus","Grape","Kiwifruit","Lettuce","Malus","Medicago","Monkeyflower",
+                        "Olive","Orange","Papaya","Pepper","Poplar","Prunus","Soybean_a4v1","Spinach",
+                        "Sugarbeet","Sunflower","Sweetpotato","Tomato_v2_4","Tomato_v3_2"],
     }
     return render(request, 'clusters/index.html', context)
 
