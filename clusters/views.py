1754edbfe8d904cf72a606ad93e5838524094b2f
堺俊之 <toshi6661024@gmail.com>
2020-01-23 23:13:08 +0000

    add context

--- clusters/views.py
+++ clusters/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return render(request, 'clusters/index.html')
+    context = {
+        'specie_list': ["Tomato_v3_2", "Tomato_v2_4"],
+    }
+    return render(request, 'clusters/index.html', context)
