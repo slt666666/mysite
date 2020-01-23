d60de0851dc9d5b585d21adcf57d5e998b7ab4fa
堺俊之 <toshi6661024@gmail.com>
2020-01-23 13:34:16 +0000

    test

--- clusters/views.py
+++ clusters/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     specie_list = {"specie_name": "Tomato_v3_2", "specie_name": "Tomato_v2_4"}
-    return render(request, 'clusters/index.html', {})
+    return render(request, 'clusters/index.html')
 
 # def detail(request, specie_name):
 #     return render(request, 'clusters/{}_cluster.html'.format(specie_name))
