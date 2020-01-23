8e704925300cece7e539dd7214ba2f1b250b8b0a
堺俊之 <toshi6661024@gmail.com>
2020-01-23 23:21:16 +0000

    fix

--- clusters/urls.py
+++ clusters/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:gene_id>/', views.detail, name='detail'),
+    path('<str:specie>/', views.detail, name='detail'),
 ]
